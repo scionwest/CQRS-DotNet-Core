c9123c54bf7e147e9811d09d97d00229350ff197
Johnathon Sullinger <scionwest@gmail.com>
2019-11-30 15:06:02 -0700

    Initial work on Kinesis Stream consumer Lambda

---
+++ src/FocusMark.Api.Projects/Consumers/CreateProjectConsumer.cs
@@ -0,0 +1,35 @@
+﻿using Amazon.Lambda.Core;
+using Amazon.Lambda.KinesisEvents;
+using System.IO;
+using System.Text;
+
+namespace FocusMark.Api.Projects.Consumers
+{
+    public class CreateProjectConsumer
+    {
+        public void FunctionHandler(KinesisEvent kinesisEvent, ILambdaContext context)
+        {
+            context.Logger.LogLine($"Beginning to process {kinesisEvent.Records.Count} records...");
+
+            foreach (var record in kinesisEvent.Records)
+            {
+                context.Logger.LogLine($"Event ID: {record.EventId}");
+                context.Logger.LogLine($"Event Name: {record.EventName}");
+
+                string recordData = GetRecordContents(record.Kinesis);
+                context.Logger.LogLine($"Record Data:");
+                context.Logger.LogLine(recordData);
+            }
+
+            context.Logger.LogLine("Stream processing complete.");
+        }
+
+        private string GetRecordContents(KinesisEvent.Record streamRecord)
+        {
+            using (var reader = new StreamReader(streamRecord.Data, Encoding.ASCII))
+            {
+                return reader.ReadToEnd();
+            }
+        }
+    }
+}
